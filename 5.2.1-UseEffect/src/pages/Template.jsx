b8026275a1e808c8074a141c1d30415f3bbb6c1d
Tonatiuh Díaz <alfredo117tdg@hotmail.com>
2024-08-14 20:48:45 -0600

    Se agrega ejemplo de generar rutas de una simulación de pegada a una api

---
+++ 5.2.1-UseEffect/src/pages/Template.jsx
@@ -0,0 +1,6 @@
+const Template = ({route}) => {
+    // Conditional rendering para saber qué tengo que ver en pantalla.
+    return <h1>{route}</h1>
+}
+
+export default Template;
--- 5.2.1-UseEffect/src/routes/RoutesIndex.jsx
+++ 5.2.1-UseEffect/src/routes/RoutesIndex.jsx
@@ -2,16 +2,31 @@ import { Routes, Route } from 'react-router-dom'
 import Home from '../pages/Home';
 import AcercaDe from '../pages/AcercaDe';
 import DetallePokemon from '../pages/DetallePokemon'
+import { useEffect, useState } from 'react';
+import Template from '../pages/Template';
 
 // El primer paso para trabajar con rutas con react-router-dom es crear un índice de rutas.
 // Esto se hace con elemento Routes importado de react-router-dom y este elemento tiene hijos de elementos Route.
 // Cada elemento Route tiene el componente a cargar y la ruta (o URL) asociada a esa pantalla.
 const RoutesIndex = () => {
+
+    const [apiRoutes, setApiRoutes] = useState([]);
+
+    useEffect(() => {
+        const response = ['/ability', '/generation'];
+        setApiRoutes(response);
+    }, []);
+
     return (
         <Routes>
             <Route path='/' element={<Home />} /> {/* Esta línea nos dice que se carge la page Home cuando estemos en la ruta / */}
             <Route path='/about' element={<AcercaDe />}/> {/* Esta línea nos dice que se carge la page AcercaDe cuando estemos en la ruta /about */}
             <Route path='/pokemonDetail/:id' element={<DetallePokemon />} />
+            {
+                apiRoutes.map((route, index) => {
+                    return <Route key={index} path={route} element={<Template route={route} />}></Route>
+                })
+            }
         </Routes>
     )
 }
