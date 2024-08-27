c470d30ef5f295968e8613ef762c0eb14c23d540
Tonatiuh Díaz <alfredo117tdg@hotmail.com>
2024-08-26 20:48:15 -0600

    Se agrega animación de cargando

--- 5.2.1-UseEffect/src/components/NavBarComponent.jsx
+++ 5.2.1-UseEffect/src/components/NavBarComponent.jsx
@@ -11,8 +11,8 @@ const NavBarComponent = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link ><NavLink to={`/`}>Home</NavLink></Nav.Link>
-              <Nav.Link ><NavLink to={`/about`}>Acerca de</NavLink></Nav.Link>
+              <Nav.Link as={NavLink} to={`/`} >Home</Nav.Link>
+              <Nav.Link as={NavLink} to={`/about`}>Acerca de</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
--- 5.2.1-UseEffect/src/pages/Home.jsx
+++ 5.2.1-UseEffect/src/pages/Home.jsx
@@ -6,6 +6,8 @@ const Home = () => {
     const [listaPokemones, setListaPokemones] = useState([]);
     const [pokemonesBuscados, setPokemonesBuscados] = useState('');
 
+    const [estaCargando, setEstaCargando] = useState(true);
+
     // El hook useEffect recibe 2 parámetros:
     /*
         1. La función a ejecutar.
@@ -18,10 +20,11 @@ const Home = () => {
     useEffect(() => {
         const consultarPokemons = async () => {
             try {
-                const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=50');
+                const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=2000');
                 const json = await response.json();
                 console.log(json);
                 setListaPokemones(json.results);
+                setEstaCargando(false);
             } catch (e) {
                 console.log(e);
             }
@@ -54,46 +57,53 @@ const Home = () => {
                     onChange={leerPokemonesBuscadosInput}
                 />
             </form>
-            <div className="container">
-                <div className="row">
-                    {
-                        pokemonesFiltrados.map((pokemon, index) => {
-                            return (
-                                /*
-                                "https://pokeapi.co/api/v2/pokemon/1/"
-                                separador = /
-                                [
-                                "https:"
-                                ""
-                                "pokeapi.co"
-                                "api"
-                                "v2"
-                                "pokemon"
-                                "1"
-                                ]
-                                */
+            {/* Conditional rendering: A través de una condición vamos a definir qué es lo que se ve en pantalla */}
+            {estaCargando ? (<div className="d-flex justify-content-center">
+                <div className="spinner-border" role="status">
+                    <span className="sr-only"></span>
+                </div>
+            </div>) :
+                <div className="container">
+                    <div className="row">
+                        {
+                            pokemonesFiltrados.map((pokemon, index) => {
+                                return (
+                                    /*
+                                    "https://pokeapi.co/api/v2/pokemon/1/"
+                                    separador = /
+                                    [
+                                    "https:"
+                                    ""
+                                    "pokeapi.co"
+                                    "api"
+                                    "v2"
+                                    "pokemon"
+                                    "1"
+                                    ]
+                                    */
 
-                                <div key={index} className="col-3 p-3">
-                                    <div className="card sombra" style={{ width: "18rem" }}>
-                                        <NavLink to={`/pokemonDetail/${pokemon.url.split('/')[6]}`}>
-                                            <img
-                                                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.url.split('/')[6]}.png`}
-                                                className="card-img-top"
-                                                alt="..."
-                                            />
-                                        </NavLink>
-                                        <div className="card-body">
+                                    <div key={index} className="col-3 p-3">
+                                        <div className="card sombra rounded-3xl" style={{ width: "18rem" }}>
                                             <NavLink to={`/pokemonDetail/${pokemon.url.split('/')[6]}`}>
-                                                <h5 className="card-title">{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</h5>
+                                                <img
+                                                    src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.url.split('/')[6]}.png`}
+                                                    className="card-img-top"
+                                                    alt="..."
+                                                />
                                             </NavLink>
+                                            <div className="card-body">
+                                                <NavLink to={`/pokemonDetail/${pokemon.url.split('/')[6]}`}>
+                                                    <h5 className="card-title">{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</h5>
+                                                </NavLink>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            );
-                        })
-                    }
+                                );
+                            })
+                        }
+                    </div>
                 </div>
-            </div>
+            }
 
 
             <NavLink to='/about'>
